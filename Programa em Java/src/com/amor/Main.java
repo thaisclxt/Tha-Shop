632f6d7c6fafabb777d0aace83e896335c004751
Thaís <thais.calixto@mail.uft.edu.br>
2020-05-09 17:53:09 -0300

    Atualização da interface inicial e do case 1

--- Programa em Java/src/com/amor/Main.java
+++ Programa em Java/src/com/amor/Main.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 
 public class Main
 {
+    public static Scanner read = new Scanner(System.in);
     public static void main(String[] args)
     {
         while (true)
         {
-            System.out.println("┌─────────────────────────────────┐");
-            System.out.println("│ ####### T H A Í S H O P ####### │");
-            System.out.println("│                                 │");
-            System.out.println("│ 1- Login como Proprietário      │");
-            System.out.println("│ 2- Login como Cliente           │");
-            System.out.println("│ 3- Login como Caixa             │");
-            System.out.println("└─────────────────────────────────┘");
+            System.out.println("┌─────────────────────────────────────────┐");
+            System.out.println("│ ########### T H A Í S H O P ########### │");
+            System.out.println("│                                         │");
+            System.out.println("│  • Menu Inicial                         │");
+            System.out.println("│                                         │");
+            System.out.println("│    1- Login como Proprietário           │");
+            System.out.println("│    2- Login como Cliente                │");
+            System.out.println("│    3- Login como Caixa                  │");
+            System.out.println("└─────────────────────────────────────────┘");
 
-            Scanner read = new Scanner(System.in);
             int option = read.nextInt();
 
             if (option != 1 & option != 2 & option != 3) { break; }
@@ -23,7 +25,40 @@ public class Main
             switch (option)
             {
                 case 1:
-                    break;
+                    while (true )
+                    {
+                        System.out.println("┌─────────────────────────────────────────┐");
+                        System.out.println("│ ########### T H A Í S H O P ########### │");
+                        System.out.println("│                                         │");
+                        System.out.println("│  • Login para Proprietário              │");
+                        System.out.println("│                                         │");
+                        System.out.println("│    Informe a senha: ********            │");
+                        System.out.println("└─────────────────────────────────────────┘");
+
+                        int password = read.nextInt();
+
+                        if (password != 84178160)
+                        {
+                            System.out.println("┌─────────────────────────────────────────┐");
+                            System.out.println("│ ########### T H A Í S H O P ########### │");
+                            System.out.println("│                                         │");
+                            System.out.println("│  • Login para Proprietário              │");
+                            System.out.println("│                                         │");
+                            System.out.println("│    Senha inválida!                      │");
+                            System.out.println("│                                         │");
+                            System.out.println("│    1- Informar a senha novamente        │");
+                            System.out.println("│    2- Voltar para o Menu Inicial        │");
+                            System.out.println("└─────────────────────────────────────────┘");
+
+                            int option2 = read.nextInt();
+
+                            if (option2 == 1) { break; }
+                            if (option2 == 2) { main(args); }
+
+                            Mercado proprietario = new Mercado();
+                        }
+                    }
+
                 case 2:
                     break;
                 case 3:

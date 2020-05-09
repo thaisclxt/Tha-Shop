01975175cdb6e5ce126e3f9fccf45161cb65317f
Thaís <thais.calixto@mail.uft.edu.br>
2020-05-09 17:05:01 -0300

    Criação da interface inicial

--- Programa em Java/src/com/amor/Main.java
+++ Programa em Java/src/com/amor/Main.java
@@ -1,9 +1,34 @@
 package com.amor;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        while (true)
+        {
+            System.out.println("┌─────────────────────────────────┐");
+            System.out.println("│ ####### T H A Í S H O P ####### │");
+            System.out.println("│                                 │");
+            System.out.println("│ 1- Login como Proprietário      │");
+            System.out.println("│ 2- Login como Cliente           │");
+            System.out.println("│ 3- Login como Caixa             │");
+            System.out.println("└─────────────────────────────────┘");
 
+            Scanner read = new Scanner(System.in);
+            int option = read.nextInt();
+
+            if (option != 1 & option != 2 & option != 3) { break; }
+
+            switch (option)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
     }
 }

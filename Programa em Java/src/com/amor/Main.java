ba346f3e8890ecebf98394e0605cd82bacac40bb
Thaís <thais.calixto@mail.uft.edu.br>
2020-05-09 21:02:34 -0300

    Criação do método "Login" e atualização da classe "Main"

--- Programa em Java/src/com/amor/Main.java
+++ Programa em Java/src/com/amor/Main.java
@@ -6,64 +6,68 @@ public class Main
     public static Scanner read = new Scanner(System.in);
     public static void main(String[] args)
     {
-        while (true)
+        System.out.println("┌─────────────────────────────────────────────┐");
+        System.out.println("│ ############# T H A Í S H O P ############# │");
+        System.out.println("│                                             │");
+        System.out.println("│  • Menu Inicial                             │");
+        System.out.println("│                                             │");
+        System.out.println("│    1- Login como Proprietário               │");
+        System.out.println("│    2- Login como Cliente                    │");
+        System.out.println("│    3- Login como Caixa                      │");
+        System.out.println("│    0- Sair do programa                      │");
+        System.out.println("└─────────────────────────────────────────────┘");
+
+        int option = read.nextInt();
+
+        switch (option)
         {
-            System.out.println("┌─────────────────────────────────────────┐");
-            System.out.println("│ ########### T H A Í S H O P ########### │");
-            System.out.println("│                                         │");
-            System.out.println("│  • Menu Inicial                         │");
-            System.out.println("│                                         │");
-            System.out.println("│    1- Login como Proprietário           │");
-            System.out.println("│    2- Login como Cliente                │");
-            System.out.println("│    3- Login como Caixa                  │");
-            System.out.println("└─────────────────────────────────────────┘");
-
-            int option = read.nextInt();
-
-            if (option != 1 & option != 2 & option != 3) { break; }
-
-            switch (option)
-            {
-                case 1:
-                    while (true )
-                    {
-                        System.out.println("┌─────────────────────────────────────────┐");
-                        System.out.println("│ ########### T H A Í S H O P ########### │");
-                        System.out.println("│                                         │");
-                        System.out.println("│  • Login para Proprietário              │");
-                        System.out.println("│                                         │");
-                        System.out.println("│    Informe a senha: ********            │");
-                        System.out.println("└─────────────────────────────────────────┘");
-
-                        int password = read.nextInt();
-
-                        if (password != 84178160)
-                        {
-                            System.out.println("┌─────────────────────────────────────────┐");
-                            System.out.println("│ ########### T H A Í S H O P ########### │");
-                            System.out.println("│                                         │");
-                            System.out.println("│  • Login para Proprietário              │");
-                            System.out.println("│                                         │");
-                            System.out.println("│    Senha inválida!                      │");
-                            System.out.println("│                                         │");
-                            System.out.println("│    1- Informar a senha novamente        │");
-                            System.out.println("│    2- Voltar para o Menu Inicial        │");
-                            System.out.println("└─────────────────────────────────────────┘");
-
-                            int option2 = read.nextInt();
-
-                            if (option2 == 1) { break; }
-                            if (option2 == 2) { main(args); }
-
-                            Mercado proprietario = new Mercado();
-                        }
-                    }
-
-                case 2:
-                    break;
-                case 3:
-                    break;
-            }
+            case 1:
+                login();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 0:
+                break;
+            default:
+                main(args);
         }
+    }
+
+    public static void login()
+    {
+        System.out.println("┌─────────────────────────────────────────────┐");
+        System.out.println("│ ############# T H A Í S H O P ############# │");
+        System.out.println("│                                             │");
+        System.out.println("│  • Login para Proprietário                  │");
+        System.out.println("│                                             │");
+        System.out.println("│    Informe a senha: ********                │");
+        System.out.println("└─────────────────────────────────────────────┘");
+
+        int password = read.nextInt();
+
+        if (password != 84178160)
+        {
+            System.out.println("┌─────────────────────────────────────────────┐");
+            System.out.println("│ ############# T H A Í S H O P ############# │");
+            System.out.println("│                                             │");
+            System.out.println("│  • Login para Proprietário                  │");
+            System.out.println("│                                             │");
+            System.out.println("│    Senha inválida!                          │");
+            System.out.println("│                                             │");
+            System.out.println("│    1- Informar a senha novamente            │");
+            System.out.println("│    2- Voltar para o Menu Inicial            │");
+            System.out.println("└─────────────────────────────────────────────┘");
+
+            int option2 = read.nextInt();
+
+            if (option2 == 1) { login(); }
+            if (option2 == 2) { main(null); }
+        }
+
+        Mercado proprietario = new Mercado();
+        proprietario.screen();
+
     }
 }
